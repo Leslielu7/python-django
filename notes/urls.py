abe6f2f4ca3080d283f06394f1c8bd573b298d7d
Leslielu7 <leslie870417@gmail.com>
2023-07-27 22:19:06 +0800

    Class based view on Note

--- notes/urls.py
+++ notes/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes',views.list),
+    path('notes',views.NotesListView.as_view()),
     # endpoint called notes, 
     #views.list is the function we created
-    path('notes/<int:pk>',views.detail)
+    path('notes/<int:pk>',views.NotesDetailView.as_view())
 
 ]
